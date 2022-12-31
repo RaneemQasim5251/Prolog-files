6f29ab37e43e150507efa9c74dcb81c71ceb0d6f
Raneem Qasim <108181104+RaneemQasim5251@users.noreply.github.com>
2022-12-31 18:33:49 +0300

    Create consecutive.pl
    compressing repeated occurrences of elements into a single copy of the element.

---
+++ consecutive.pl
@@ -0,0 +1,10 @@
+% compress(L,S) :- the list S is obtained from the
+% list L by compressing repeated occurrences of elements into a single copy of the element.
+
+compress([],[]). % Base Case: L is empty []
+
+compress([X],[X]). % Base Case: L only one element
+
+compress([X,X|Xs],S):- compress([X|Xs],S).
+
+compress([X,Y|Ys],[X|S]) :- X \= Y, compress([Y|Ys],S). % \= means (differents from)
