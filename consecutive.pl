65760c51e4868115e6ef3c850c1c1ef7e924849c
Raneem Qasim <108181104+RaneemQasim5251@users.noreply.github.com>
2022-12-31 18:39:04 +0300

    Update consecutive.pl

--- consecutive.pl
+++ consecutive.pl
@@ -3,8 +3,8 @@
 
 compress([],[]). % Base Case: L is empty []
 
-compress([X],[X]). % Base Case: L only one element
+compress([X],[X]). % Base Case: L only one element.
 
-compress([X,X|Xs],S):- compress([X|Xs],S).
+compress([X,X|Xs],S):- compress([X|Xs],S). 
 
 compress([X,Y|Ys],[X|S]) :- X \= Y, compress([Y|Ys],S). % \= means (differents from)
