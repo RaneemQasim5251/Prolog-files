b95bc32fcc1ccac40ad4809fc0795cfe75a7ff04
Raneem Qasim <108181104+RaneemQasim5251@users.noreply.github.com>
2023-01-04 23:42:05 +0300

    Update scalar.pl

--- scalar.pl
+++ scalar.pl
@@ -2,9 +2,10 @@
 
 pescalar([],[],0).  % BASE CASE - both lists empty
 
-pescalar([L1|List1],[L2|List2],R):  pescalar(List1,List2,R1), R is R1 + L1*L2.
+pescalar([L1|List1],[L2|List2],R):-
+pescalar(List1,List2,R1), R is R1 + L1*L2.
 
 % L = [L1|List1] = [5,2,1]
-% Q = [L2|List2] = [3,2,3
+% Q = [L2|List2] = [3,2,3]
 % R1 = 7
 % L1*L2 = 15
